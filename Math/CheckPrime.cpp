3178c76061d6d9252f968dfea4cdf25641b68965
Harsh <31737253+harsh-0706@users.noreply.github.com>
2019-07-21 13:24:01 +0530

    Update CheckPrime.cpp

--- Math/CheckPrime.cpp
+++ Math/CheckPrime.cpp
@@ -4,7 +4,7 @@ bool isprime(int x)
   if(x<=1) return false;
   if(x<=3) return true;
   if (!(x%2) || !(x%3)) return false;
-  int s=(int)(sqrt((double)(x))+EPS);
+  int s = (int)(sqrt((double)(x))+EPS);
   for(int i=5; i<=s; i+=6)
   {
     if (!(x%i) || !(x%(i+2))) return false;
