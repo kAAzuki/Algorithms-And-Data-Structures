19441cb7d65d1f43e6671760bea801b1620d8231
Harsh <31737253+harsh-0706@users.noreply.github.com>
2019-07-21 13:22:12 +0530

    Create CheckPrime.cpp

---
+++ Math/CheckPrime.cpp
@@ -0,0 +1,13 @@
+#define EPS 1e-7
+bool isprime(int x)
+{
+  if(x<=1) return false;
+  if(x<=3) return true;
+  if (!(x%2) || !(x%3)) return false;
+  int s=(int)(sqrt((double)(x))+EPS);
+  for(int i=5; i<=s; i+=6)
+  {
+    if (!(x%i) || !(x%(i+2))) return false;
+  }
+  return true;
+}
